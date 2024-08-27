4a759b19d8af3d7edc4d27a14af4813404ed0644
pablogila <pablo.gila.herranz@gmail.com>
2024-08-27 18:33:41 +0200

    commit before messing too much

--- maps/TileMapDual.gd
+++ maps/TileMapDual.gd
@@ -7,9 +7,9 @@ extends TileMapLayer
 ## with respect to the World grid.
 @export var world_tilemap: TileMapLayer
 ## Coordinates for the tile in the Atlas
-## that will be used to draw in the World TileMapLayer.
+## that will be used to sketch in-editor in the World TileMapLayer.
 ## Defaults to the one in the official Godot template.
-@export var draw_atlas_coord: Vector2i = Vector2i(2,1)
+@export var sketch_atlas_coord: Vector2i = Vector2i(2,1)
 
 ## Bit-wise logic: summing over all neighbours
 ## provides the proper tile from the Atlas.
@@ -53,6 +53,7 @@ const NEIGHBOURS_TO_ATLAS: Dictionary = {
 
 func _ready() -> void:
 	world_tilemap.changed.connect(_update_tilemap)
+	_update_tiles()
 
 
 func _update_tilemap() -> void:
@@ -60,35 +61,56 @@ func _update_tilemap() -> void:
 	self.position.x = - self.tile_set.tile_size.x * 0.5
 	self.position.y = - self.tile_set.tile_size.y * 0.5
 	_update_tiles()
+	print('TileMap updated')
 
 
 func _update_tiles() -> void:
-	var _cell_coords: Vector2i
-	var _cell_atlas_coords: Vector2i
-	for _cell in self.get_used_cells():
-		_cell_coords = Vector2i(0,0) # placeholder
-		_cell_atlas_coords = _calculate_display_tile(_cell_coords)
-		self.set_cell(_cell_coords, 0, _cell_atlas_coords)
+	for _world_cell in world_tilemap.get_used_cells():
+		if _is_world_tile_sketched(_world_cell):
+			print(_world_cell)
+			_update_tiles_around_world_tile(_world_cell)
 
 
-func _calculate_display_tile(coords: Vector2i) -> Vector2i:
-	var top_left = _is_world_tile_drawn(coords - NEIGHBOURS.direction.TOP_LEFT)
-	var low_left = _is_world_tile_drawn(coords - NEIGHBOURS.direction.LOW_LEFT)
-	var top_right = _is_world_tile_drawn(coords - NEIGHBOURS.direction.TOP_RIGHT)
-	var low_right = _is_world_tile_drawn(coords - NEIGHBOURS.direction.LOW_RIGHT)
-
-	var tile_key = 0
-	tile_key += top_left * NEIGHBOURS.direction.TOP_LEFT
-	tile_key += low_left * NEIGHBOURS.direction.LOW_LEFT
-	tile_key += top_right * NEIGHBOURS.direction.TOP_RIGHT
-	tile_key += low_right * NEIGHBOURS.direction.LOW_RIGHT
-	
-	return NEIGHBOURS_TO_ATLAS[tile_key]
-
-
-func _is_world_tile_drawn(coords: Vector2i) -> int:
+func _is_world_tile_sketched(coords: Vector2i) -> bool:
 	var atlas_coord = get_cell_atlas_coords(coords)
-	if atlas_coord == draw_atlas_coord:
-		return 1
+	if atlas_coord == sketch_atlas_coord:
+		return true
 	else:
-		return 0
+		return false
+
+
+func _update_tiles_around_world_tile(_coords: Vector2i) -> void:
+	for _key in NEIGHBOURS:
+		var _displaced_tile = _coords + NEIGHBOURS[_key]
+		_update_tile_at_coord(_displaced_tile)
+	
+	#var top_left = _coords + NEIGHBOURS.direction.TOP_LEFT
+	#var low_left = _coords + NEIGHBOURS.direction.LOW_LEFT
+	#var top_right = _coords + NEIGHBOURS.direction.TOP_RIGHT
+	#var low_right = _coords + NEIGHBOURS.direction.LOW_RIGHT
+	#_update_tile_at_coord(top_left)
+	#_update_tile_at_coord(low_left)
+	#_update_tile_at_coord(top_right)
+	#_update_tile_at_coord(low_right)
+
+
+func _update_tile_at_coord(_coords: Vector2i) -> void:
+	var _world_top_left: Vector2i = _coords + NEIGHBOURS.direction.TOP_LEFT
+	var _world_low_left: Vector2i = _coords + NEIGHBOURS.direction.LOW_LEFT
+	var _world_top_right: Vector2i = _coords + NEIGHBOURS.direction.TOP_RIGHT
+	var _world_low_right: Vector2i = _coords + NEIGHBOURS.direction.LOW_RIGHT
+	
+	var _coords_atlas: Vector2i
+	var _tile_key: int = 0
+	if _is_world_tile_sketched(_world_top_left):
+		_tile_key += direction.TOP_LEFT
+	if _is_world_tile_sketched(_world_low_left):
+		_tile_key += direction.LOW_LEFT
+	if _is_world_tile_sketched(_world_top_right):
+		_tile_key += direction.TOP_RIGHT
+	if _is_world_tile_sketched(_world_low_right):
+		_tile_key += direction.LOW_RIGHT
+	
+	_coords_atlas = NEIGHBOURS_TO_ATLAS[_tile_key]
+	self.set_cell(_coords, 0, _coords_atlas)
+	
