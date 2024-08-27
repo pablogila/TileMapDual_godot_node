9cc44d89eca0c5f644ff6001dd6eeed39ca16387
pablogila <pablo.gila.herranz@gmail.com>
2024-08-28 01:02:39 +0200

    working

--- README.md
+++ README.md
@@ -1,25 +1,59 @@
-# TileMapDual Node
+# TileMapDual Godot Node
 
 Introducing TileMapDual, a custom TileMapLayer node
-which provides a real-time, in-editor dual-grid tileset system for Godot.
-This reduces the number of tilesets required from 47 to just 15,
-which greatly speeds up your dev journey.
+which provides a real-time, in-editor and in-game dual-grid tileset system for Godot.
+This dual-grid system [proposed by Oskar Stålberg](https://x.com/OskSta/status/1448248658865049605) reduces the number of tiles required from 47 to just 15 (yes, fifteen!!), rocketing your dev journey!  
+
+![](docs/reference_dual.jpeg)
+
+## Advatages
+
+Using a dual-grid system has the following advantages:  
+- Only 15 tiles are required, instead of 47
+- The tiles can have perfectly rounded corners
+- The tiles align to the world grid
+
+## Installation and usage
+
+Just copy the `TileMapDual.gd` script (Inside the TileMapDual folder) to your project to start using the new custom node.  
+
+You have to create a regular `TileMapLayer` with your own 2x2 tileset, following the [standard godot tileset](https://user-images.githubusercontent.com/47016402/87044518-ee28fa80-c1f6-11ea-86f5-de53e86fcbb6.png).
+You just need to quickly sketch your level with the fully filled tile, indicated here:
+
+![](docs/reference_tileset_standard.png)
+
+Then, create a `TileMapDual` node and assign the `TileMapLayer` to it. Just click the `Update in Editor` checkbox, and the dual grid will be automatically configured and generated in real-time.  
+Any change in the `TileMapLayer` will be updated by simply clicking the checkbox again!
+
+An in-game implementation can be activated by ckecking the `Update in Game` setting. This will update the dual grid in real-time during gameplay, thanks to the `TileMapLayer.changed` signal.
 
 ## Why?
+
 Previous implementations of a dual-grid tileset system in Godot
 by [GlitchedInOrbit](https://github.com/GlitchedinOrbit/dual-grid-tilemap-system-godot-gdscript)
-and [jess-hammer](https://github.com/jess-hammer/dual-grid-tilemap-system-godot)
-used an inverted version of the official 16-tile template.
-This is a potential source of headaches, and this release corrects that inversion.  
-This version also implements modern TileMapLayers instead of the deprecated TileMap node.  
-Finally, and most importantly, this release introduces the dual-grid system as a custom node
-that runs within the editor, making it easy to integrate into your own projects.  
+and [jess:codes](https://github.com/jess-hammer/dual-grid-tilemap-system-godot)
+used an inverted version of the official 16-tile template (although Jess's tileset is provided as an example in this repo).
+This is a potential source of headaches, and this release corrects said inversion.  
 
-## TO-DO
-- STILL IN DEVELOPMENT
-- Isometric support
+This release also implements modern TileMapLayers instead of the deprecated TileMap node.  
+
+Finally, and most importantly, this release simplifies the process by introducing the dual-grid system as a simple custom node that runs within the editor, making it easy to integrate into your own projects.  
+
+## To-do
+
+- Implement isometric support
 
 ## References
+
 - [Dual grid Twitter post by Oskar Stålberg](https://x.com/OskSta/status/1448248658865049605)
 - ['Programming Terrain Generation' video by ThinMatrix](https://www.youtube.com/watch?v=buKQjkad2I0)
 - ['Drawing Fewer Tiles' video by jess:codes](https://www.youtube.com/watch?v=jEWFSv3ivTg)
+- [jess:codes implementation in C#](https://github.com/jess-hammer/dual-grid-tilemap-system-godot)
+- [GlitchedInOrbit implementation in GDScript](https://github.com/GlitchedinOrbit/dual-grid-tilemap-system-godot-gdscript)
+
+## Feedback
+
+Please feel free to contact me to provide feedback, suggestions, or improvements to this project.  
+- [Twitter (@GilaPixel)](https://x.com/gilapixel)
+- [Instagram (@GilaPixel)](https://www.instagram.com/gilapixel/)
+- [Reddit (/u/pgilah)](https://www.reddit.com/u/pgilah/)
--- TileMapDual/TileMapDual.gd
+++ TileMapDual/TileMapDual.gd
@@ -2,14 +2,27 @@
 class_name TileMapDual
 extends TileMapLayer
 
-@export var debug: bool = false
-## World TileMapLayer, where the tiles are sketched.
+## TileMapLayer in the World grid, where the tiles are sketched.
 ## An offset of (-0.5,-0.5) tiles will be applied
 ## with respect to the World grid.
-@export var world_tilemap: TileMapLayer
-## coordinates for the tile in the Atlas
+## Sketch with the corresponding sketch_atlas_coord.
+@export var sketch_tilemap: TileMapLayer = null
+## Click to update the tilemap inside the editor.
+@export var update_in_editor: bool = false:
+	set(value):
+		_update_tilemap()
+## Update and modify the tileset in-game via sketch_tilemap.changed() signal.
+## Disable it to freeze the tilemap in its current state.
+@export var update_in_game: bool = false
+## Clean all the tiles from the TileMapDual node.
+@export var clean: bool = false:
+	set(value):
+		self.clear()
+## Print debug messages. Lots of them.
+@export var debug: bool = false
+## Coordinates for the tile in the Atlas
 ## that will be used to sketch in-editor in the World TileMapLayer.
-## Defaults to the one in the official Godot template.
+## Defaults to the one in the standard Godot template.
 @export var sketch_atlas_coords: Vector2i = Vector2i(2,1)
 
 ## Bit-wise logic: summing over all neighbours
@@ -34,40 +47,48 @@ const NEIGHBOURS := {
 ## summation over all sketched NEIGHBOURS.
 ## Follows the official 2x2 template.
 const NEIGHBOURS_TO_ATLAS: Dictionary = {
-	 0: Vector2i(3,0),
+	 0: Vector2i(0,3),
 	 1: Vector2i(3,3),
 	 2: Vector2i(0,0),
 	 3: Vector2i(3,2),
-	 4: Vector2i(0,1),
+	 4: Vector2i(0,2),
 	 5: Vector2i(1,2),
 	 6: Vector2i(2,3),
 	 7: Vector2i(3,1),
 	 8: Vector2i(1,3),
 	 9: Vector2i(0,1),
-	10: Vector2i(0,3),
-	11: Vector2i(0,2),
-	12: Vector2i(0,1),
+	10: Vector2i(3,0),
+	11: Vector2i(2,0),
+	12: Vector2i(1,0),
 	13: Vector2i(2,2),
 	14: Vector2i(1,1),
 	15: Vector2i(2,1)
 	}
 
+var checked_cells: Array = []
+
 
 func _ready() -> void:
-	world_tilemap.changed.connect(_update_tilemap)
+	if update_in_game:
+		if debug:
+			print('Updating in-game is activated')
+		sketch_tilemap.changed.connect(_update_tilemap)
+		_update_tilemap()
 
 
 func _update_tilemap() -> void:
-	self.tile_set = world_tilemap.tile_set
+	self.tile_set = sketch_tilemap.tile_set
 	self.position.x = - self.tile_set.tile_size.x * 0.5
 	self.position.y = - self.tile_set.tile_size.y * 0.5
+	self.clear()
 	_update_tiles()
 
 
 func _update_tiles() -> void:
+	checked_cells = []
 	if debug:
 		print('Updating tiles....................')
-	for _world_cell in world_tilemap.get_used_cells():
+	for _world_cell in sketch_tilemap.get_used_cells():
 		_update_tiles_around_world_cell(_world_cell)
 
 
@@ -78,25 +99,28 @@ func _update_tiles_around_world_cell(_world_cell: Vector2i) -> void:
 	var _low_left = _world_cell + NEIGHBOURS[location.LOW_LEFT]
 	var _top_right = _world_cell + NEIGHBOURS[location.TOP_RIGHT]
 	var _low_right = _world_cell + NEIGHBOURS[location.LOW_RIGHT]
-	_update_tile_at_coords(_top_left)
-	_update_tile_at_coords(_low_left)
-	_update_tile_at_coords(_top_right)
-	_update_tile_at_coords(_low_right)
+	_update_cell(_top_left)
+	_update_cell(_low_left)
+	_update_cell(_top_right)
+	_update_cell(_low_right)
 
 
-func _update_tile_at_coords(_coords: Vector2i) -> void:
+func _update_cell(_cell: Vector2i) -> void:
+	# Avoid upgrading cells more than necessary
+	if _cell in checked_cells:
+		return
+	checked_cells.append(_cell)
 	
 	if debug:
-		print('    Checking display tile ' + str(_coords) + '...')
+		print('    Checking display tile ' + str(_cell) + '...')
+	
+	var _top_left = _cell - NEIGHBOURS[location.LOW_RIGHT]
+	var _low_left = _cell - NEIGHBOURS[location.TOP_RIGHT]
+	var _top_right = _cell - NEIGHBOURS[location.LOW_LEFT]
+	var _low_right = _cell - NEIGHBOURS[location.TOP_LEFT]
 	
 	var _coords_atlas: Vector2i
 	var _tile_key: int = 0
-	
-	var _top_left = _coords - NEIGHBOURS[location.LOW_RIGHT]
-	var _low_left = _coords - NEIGHBOURS[location.TOP_RIGHT]
-	var _top_right = _coords - NEIGHBOURS[location.LOW_LEFT]
-	var _low_right = _coords - NEIGHBOURS[location.TOP_LEFT]
-	
 	if _is_world_tile_sketched(_top_left):
 		_tile_key += location.TOP_LEFT
 	if _is_world_tile_sketched(_low_left):
@@ -106,27 +130,23 @@ func _update_tile_at_coords(_coords: Vector2i) -> void:
 	if _is_world_tile_sketched(_low_right):
 		_tile_key += location.LOW_RIGHT
 	
-	#for _key in NEIGHBOURS:
-	#	var _world_cell = _coords + NEIGHBOURS[_key]
-	#	if _is_world_tile_sketched(_world_cell):
-	#		_tile_key += _key
-	
 	_coords_atlas = NEIGHBOURS_TO_ATLAS[_tile_key]
-	self.set_cell(_coords, 0, _coords_atlas)
+	self.set_cell(_cell, 0, _coords_atlas)
 	if debug:
-		print('    Display tile ' + str(_coords) + ' updated with key ' + str(_tile_key))
+		print('    Display tile ' + str(_cell) + ' updated with key ' + str(_tile_key))
 
 
-func _is_world_tile_sketched(_coords: Vector2i) -> bool:
-	var _atlas_coords = world_tilemap.get_cell_atlas_coords(_coords)
+func _is_world_tile_sketched(_world_cell: Vector2i) -> bool:
+	var _atlas_coords = sketch_tilemap.get_cell_atlas_coords(_world_cell)
 	if _atlas_coords == sketch_atlas_coords:
 		if debug:
-			print('      World cell ' + str(_coords) + ' IS sketched with atlas coords ' + str(_atlas_coords))
+			print('      World cell ' + str(_world_cell) + ' IS sketched with atlas coords ' + str(_atlas_coords))
 		return true
 	else:
 		if Vector2(_atlas_coords) == Vector2(-1,-1):
-			print('      World cell ' + str(_coords) + ' Is NOT sketched')
+			if debug:
+				print('      World cell ' + str(_world_cell) + ' Is EMPTY')
 			return false
 		if debug:
-			print('      World cell ' + str(_coords) + ' Is NOT sketched with atlas coords ' + str(_atlas_coords))
+			print('      World cell ' + str(_world_cell) + ' Is NOT sketched with atlas coords ' + str(_atlas_coords))
 		return false
