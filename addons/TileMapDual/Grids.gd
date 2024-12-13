32635f7d49ed415b5f02005df16d9fcc0a623cc3
raffimolero <raffimolero@gmail.com>
2024-12-14 01:40:44 +0800

    restructure GRID_DATA again

--- addons/TileMapDual/Grids.gd
+++ addons/TileMapDual/Grids.gd
@@ -37,190 +37,178 @@ static func grid_data(tile_set: TileSet) -> Dictionary:
 ## summation over all sketched NEIGHBOURS.
 const TEMPLATES: Dictionary = {
 	'square': [
-		[
-			Vector2i(0, 3),
-			Vector2i(1, 3),
-			Vector2i(0, 0),
-			Vector2i(3, 0),
-			Vector2i(3, 3),
-			Vector2i(0, 1),
-			Vector2i(3, 2),
-			Vector2i(2, 0),
-			Vector2i(0, 2),
-			Vector2i(1, 0),
-			Vector2i(2, 3),
-			Vector2i(1, 1),
-			Vector2i(1, 2),
-			Vector2i(2, 2),
-			Vector2i(3, 1),
-			Vector2i(2, 1),
-		],
+		Vector2i(0, 3),
+		Vector2i(1, 3),
+		Vector2i(0, 0),
+		Vector2i(3, 0),
+		Vector2i(3, 3),
+		Vector2i(0, 1),
+		Vector2i(3, 2),
+		Vector2i(2, 0),
+		Vector2i(0, 2),
+		Vector2i(1, 0),
+		Vector2i(2, 3),
+		Vector2i(1, 1),
+		Vector2i(1, 2),
+		Vector2i(2, 2),
+		Vector2i(3, 1),
+		Vector2i(2, 1),
 	],
-	'triangle_vertical': [
-		[ # >
-			Vector2i(0, 0),
-			Vector2i(0, 2),
-			Vector2i(1, 1),
-			Vector2i(3, 3),
-			Vector2i(1, 3),
-			Vector2i(3, 1),
-			Vector2i(2, 2),
-			Vector2i(2, 0),
-		], [ # <
-			Vector2i(1, 0),
-			Vector2i(0, 1),
-			Vector2i(1, 2),
-			Vector2i(2, 3),
-			Vector2i(0, 3),
-			Vector2i(3, 2),
-			Vector2i(2, 1),
-			Vector2i(3, 0),
-		],
+	'triangle_vertical_right': [
+		Vector2i(0, 0),
+		Vector2i(0, 2),
+		Vector2i(1, 1),
+		Vector2i(3, 3),
+		Vector2i(1, 3),
+		Vector2i(3, 1),
+		Vector2i(2, 2),
+		Vector2i(2, 0),
 	],
-	'triangle_horizontal': [
-		[ # v
-			Vector2i(0, 0),
-			Vector2i(2, 0),
-			Vector2i(1, 1),
-			Vector2i(3, 3),
-			Vector2i(3, 1),
-			Vector2i(1, 3),
-			Vector2i(2, 2),
-			Vector2i(0, 2),
-		], [# ^
-			Vector2i(0, 1),
-			Vector2i(1, 0),
-			Vector2i(2, 1),
-			Vector2i(3, 2),
-			Vector2i(3, 0),
-			Vector2i(2, 3),
-			Vector2i(1, 2),
-			Vector2i(0, 3),
-		],
+	'triangle_vertical_left': [ # <
+		Vector2i(1, 0),
+		Vector2i(0, 1),
+		Vector2i(1, 2),
+		Vector2i(2, 3),
+		Vector2i(0, 3),
+		Vector2i(3, 2),
+		Vector2i(2, 1),
+		Vector2i(3, 0),
+	],
+	'triangle_horizontal_down': [
+		Vector2i(0, 0),
+		Vector2i(2, 0),
+		Vector2i(1, 1),
+		Vector2i(3, 3),
+		Vector2i(3, 1),
+		Vector2i(1, 3),
+		Vector2i(2, 2),
+		Vector2i(0, 2),
+	],
+	'triangle_horizontal_up': [
+		Vector2i(0, 1),
+		Vector2i(1, 0),
+		Vector2i(2, 1),
+		Vector2i(3, 2),
+		Vector2i(3, 0),
+		Vector2i(2, 3),
+		Vector2i(1, 2),
+		Vector2i(0, 3),
 	],
 }
 
 
 ## How to deal with every available GridShape.
 const GRID_DATA = [
-	{ # GridShape.SQUARE
+	[{ # GridShape.SQUARE
 		'template': TEMPLATES.square,
-		'displays': [{
-			'offset': Vector2(-0.5, -0.5),
+		'offset': Vector2(-0.5, -0.5),
+		'dual_to_display': [
+			TileSet.CELL_NEIGHBOR_RIGHT_SIDE,
+			TileSet.CELL_NEIGHBOR_BOTTOM_RIGHT_CORNER,
+			TileSet.CELL_NEIGHBOR_BOTTOM_SIDE,
+		],
+		'display_to_dual': [
+			TileSet.CELL_NEIGHBOR_LEFT_SIDE,
+			TileSet.CELL_NEIGHBOR_TOP_LEFT_CORNER,
+			TileSet.CELL_NEIGHBOR_TOP_SIDE,
+		],
+	}],
+	[{ # GridShape.ISO
+		'template': TEMPLATES.square,
+		'offset': Vector2(0, -0.5),
+		'dual_to_display': [
+			TileSet.CELL_NEIGHBOR_BOTTOM_RIGHT_SIDE,
+			TileSet.CELL_NEIGHBOR_BOTTOM_CORNER,
+			TileSet.CELL_NEIGHBOR_BOTTOM_LEFT_SIDE,
+		],
+		'display_to_dual': [
+			TileSet.CELL_NEIGHBOR_TOP_LEFT_SIDE,
+			TileSet.CELL_NEIGHBOR_TOP_CORNER,
+			TileSet.CELL_NEIGHBOR_TOP_RIGHT_SIDE,
+		],
+	}],
+	[ # GridShape.HALF_OFF_HORI
+		{
+			'template': TEMPLATES.triangle_horizontal_down,
+			'offset': TODO,
 			'dual_to_display': [
-				TileSet.CELL_NEIGHBOR_RIGHT_SIDE,
-				TileSet.CELL_NEIGHBOR_BOTTOM_RIGHT_CORNER,
-				TileSet.CELL_NEIGHBOR_BOTTOM_SIDE,
+				TODO,
 			],
 			'display_to_dual': [
-				TileSet.CELL_NEIGHBOR_LEFT_SIDE,
-				TileSet.CELL_NEIGHBOR_TOP_LEFT_CORNER,
-				TileSet.CELL_NEIGHBOR_TOP_SIDE,
+				TODO,
 			],
-		}],
-	},
-	{ # GridShape.ISO
-		'template': TEMPLATES.square,
-		'displays': [{
-			'offset': Vector2(0, -0.5),
+		}, {
+			'template': TEMPLATES.triangle_horizontal_up,
+			'offset': TODO,
 			'dual_to_display': [
-				TileSet.CELL_NEIGHBOR_BOTTOM_RIGHT_SIDE,
-				TileSet.CELL_NEIGHBOR_BOTTOM_CORNER,
-				TileSet.CELL_NEIGHBOR_BOTTOM_LEFT_SIDE,
+				TODO,
 			],
 			'display_to_dual': [
-				TileSet.CELL_NEIGHBOR_TOP_LEFT_SIDE,
-				TileSet.CELL_NEIGHBOR_TOP_CORNER,
-				TileSet.CELL_NEIGHBOR_TOP_RIGHT_SIDE,
+				TODO,
 			],
-		}],
-	},
-	{ # GridShape.HALF_OFF_HORI
-		'template': TEMPLATES.triangle_horizontal,
-		'displays': [
-			{
-				'offset': TODO,
-				'dual_to_display': [
-					TODO,
-				],
-				'display_to_dual': [
-					TODO,
-				],
-			}, {
-				'offset': TODO,
-				'dual_to_display': [
-					TODO,
-				],
-				'display_to_dual': [
-					TODO,
-				],
-			},
-		],
-	},
-	{ # GridShape.HALF_OFF_VERT
-		'template': TEMPLATES.triangle_vertical,
-		'displays': [
-			{
-				'offset': TODO,
-				'dual_to_display': [
-					TODO,
-				],
-				'display_to_dual': [
-					TODO,
-				],
-			}, {
-				'offset': TODO,
-				'dual_to_display': [
-					TODO,
-				],
-				'display_to_dual': [
-					TODO,
-				],
-			},
-		],
-	},
-	{ # GridShape.HEX_HORI
-		'template': TEMPLATES.triangle_horizontal,
-		'displays': [
-			{
-				'offset': TODO,
-				'dual_to_display': [
-					TODO,
-				],
-				'display_to_dual': [
-					TODO,
-				],
-			}, {
-				'offset': TODO,
-				'dual_to_display': [
-					TODO,
-				],
-				'display_to_dual': [
-					TODO,
-				],
-			},
-		],
-	},
-	{ # GridShape.HEX_VERT
-		'template': TEMPLATES.triangle_vertical,
-		'displays': [
-			{
-				'offset': Vector2(-0.25 / sqrt(3), -0.25),
-				'dual_to_display': [
-					TODO,
-				],
-				'display_to_dual': [
-					TODO,
-				],
-			}, {
-				'offset': Vector2(-0.25 / sqrt(3), -0.75),
-				'dual_to_display': [
-					TODO,
-				],
-				'display_to_dual': [
-					TODO,
-				],
-			},
-		],
-	},
+		},
+	],
+	[ # GridShape.HALF_OFF_VERT
+		{
+			'template': TEMPLATES.triangle_vertical_right,
+			'offset': TODO,
+			'dual_to_display': [
+				TODO,
+			],
+			'display_to_dual': [
+				TODO,
+			],
+		}, {
+			'template': TEMPLATES.triangle_vertical_left,
+			'offset': TODO,
+			'dual_to_display': [
+				TODO,
+			],
+			'display_to_dual': [
+				TODO,
+			],
+		},
+	],
+	[ # GridShape.HEX_HORI
+		{
+			'template': TEMPLATES.triangle_horizontal_down,
+			'offset': TODO,
+			'dual_to_display': [
+				TODO,
+			],
+			'display_to_dual': [
+				TODO,
+			],
+		}, {
+			'template': TEMPLATES.triangle_horizontal_up,
+			'offset': TODO,
+			'dual_to_display': [
+				TODO,
+			],
+			'display_to_dual': [
+				TODO,
+			],
+		},
+	],
+	[ # GridShape.HEX_VERT
+		{
+			'template': TEMPLATES.triangle_vertical_right,
+			'offset': Vector2(-0.25 / sqrt(3), -0.25),
+			'dual_to_display': [
+				TODO,
+			],
+			'display_to_dual': [
+				TODO,
+			],
+		}, {
+			'template': TEMPLATES.triangle_vertical_left,
+			'offset': Vector2(-0.25 / sqrt(3), -0.75),
+			'dual_to_display': [
+				TODO,
+			],
+			'display_to_dual': [
+				TODO,
+			],
+		},
+	],
 ]
